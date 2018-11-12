cb9761f6d346dfaf1c6dcf4f7fffc3d34b2d3e45
Ian T <ipt2396@gmail.com>
2018-11-12 15:14:10 -0500

    Code to open files on button click. Forces .dbc files to be selected

--- CarReportGenerator/CarReportGenerator/Form1.cs
+++ CarReportGenerator/CarReportGenerator/Form1.cs
@@ -17,9 +17,31 @@ namespace CarReportGenerator
             InitializeComponent();
         }
 
+        OpenFileDialog ofd = new OpenFileDialog();
         private void button1_Click(object sender, EventArgs e)
         {
+            ofd.Filter = "dbc files|*.dbc"; //only allows dbc file extensions
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
 
+                System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName);
+                richTextBox1.Text = sr.ReadToEnd().ToString();
+                //MessageBox.Show(sr.ReadToEnd());
+                sr.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "dbc files|*.dbc"; //only allows dbc file extensions
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName);
+                richTextBox2.Text = sr.ReadToEnd().ToString();
+                //MessageBox.Show(sr.ReadToEnd());
+                sr.Close();
+            }
         }
     }
 }
